de6e15eb99a3cfa602be746a795249010bd91d5b
shiranjit <shirishranjit@gmail.com>
2023-03-28 09:55:22 -0400

    Update streamlit_app.py
    added a button to get all the list and moved the stop below.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,19 +44,26 @@ try:
 except URLError as e:
   stremlit.error()
   
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("Hello from Snowflake:")
 streamlit.header ("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 
 # New Section to have a selection
